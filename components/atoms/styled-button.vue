4763a77b63d4e613abad0c7166f9f9c480f243ea
sampo02 <m.nishitsuji@gmail.com>
2020-04-09 20:53:40 +0900

    :+1: Remove button shadow

--- components/atoms/styled-button.vue
+++ components/atoms/styled-button.vue
@@ -15,14 +15,12 @@ export default Vue.extend({
 
 <style scoped>
 .styled-button {
-  width: 221px;
-  height: 68px;
+  width: 160px;
+  height: 48px;
   text-align: center;
   background-color: #685F56;
-  color: #ffffff;
-  font-size: 1.2rem; 
-  line-height: 68px;
-  border-radius: 33px;
-  box-shadow: 10px 10px 20px #685f56;
+  color: #FFFFFF;
+  font-size: 1.0rem; 
+  border-radius: 32px;
 }
 </style>
