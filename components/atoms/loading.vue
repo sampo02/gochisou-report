2224f314763b9ce57e88e037a54e2d824506e21f
sampo02 <m.nishitsuji@gmail.com>
2020-08-14 20:58:01 +0900

    :+1: Introduce storybook (#28)

--- components/atoms/loading.vue
+++ components/atoms/loading.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img class="loading" src="@/assets/loading.gif" />
+    <img src="@/assets/loading.gif" />
   </div>
 </template>
 
@@ -10,7 +10,7 @@ export default Vue.extend({})
 </script>
 
 <style scoped>
-.loading {
+img {
   height: 80px;
   width: 80px;
 }
