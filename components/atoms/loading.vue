40fe9f89d685269099f8cae1fc3f4a201fe88d68
sampo02 <m.nishitsuji@gmail.com>
2021-07-03 23:40:29 +0900

    🔧  Setup Composition API and tsx

--- components/atoms/loading.vue
+++ components/atoms/loading.vue
@@ -1,12 +1,15 @@
-<template>
-  <div>
-    <img class="loading" src="@/assets/loading.gif" />
-  </div>
-</template>
-
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({})
+<script lang="tsx">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    const loadingIcon = require('../../assets/loading.gif')
+    return () => (
+      <div>
+        <img class="loading" src={loadingIcon} alt="loading" />
+      </div>
+    )
+  },
+})
 </script>
 
 <style scoped>
